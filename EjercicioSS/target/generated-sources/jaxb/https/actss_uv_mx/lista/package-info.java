fd047f16c6ea07c927cecee127d706658ba7f9ec
ArisbethGM <71732318+ArisbethGM@users.noreply.github.com>
2022-04-21 00:14:45 -0500

    Delete EjercicioSS directory

--- EjercicioSS/target/generated-sources/jaxb/https/actss_uv_mx/lista/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.3.2 
-// Visite <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
-// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2022.04.19 a las 12:48:20 PM CDT 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "https://actss.uv.mx/lista", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package https.actss_uv_mx.lista;
